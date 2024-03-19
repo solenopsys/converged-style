2af79cde80ec72df06e1c266884fa1e71a561dbc
Alex Storm <info@alexstorm.ru>
2024-03-19 20:48:52 +0530

    fix build

--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,14 @@ import presetUno from "@unocss/preset-uno";
 import type { RuntimeContext } from "./config";
 import init from "./config";
 
-import presetMini from '@unocss/preset-mini'
-import presetAttributify from '@unocss/preset-attributify'
-
+import presetMini from "@unocss/preset-mini";
+import presetAttributify from "@unocss/preset-attributify";
 
 init({
-  defaults: {
-    presets: [
-      presetMini(),
-      presetAttributify(),
-    ],
-  },
-})
+	defaults: {
+		presets: [presetMini(), presetAttributify()],
+	},
+});
 
 window.__unocss_runtime = window.__unocss_runtime ?? ({} as RuntimeContext);
 window.__unocss_runtime.presets = Object.assign(
